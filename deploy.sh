94ccd3af15459c086102030d2f8cd04b3c7b48b9
Kam Lasater <kam@cyclic.sh>
2021-07-13 14:17:28 -0400

    Staging last changes

--- deploy.sh
+++ deploy.sh
@@ -13,3 +13,12 @@ aws cloudformation deploy \
     --template-file ./spike-infra-template.yaml \
     --parameter-overrides ParamWebhookUrl=$1 \
     --stack-name spike-sns-alert
+
+cfn-lint -t budget-template.yaml
+
+# aws cloudformation validate-template \
+#     --template-body ./template.yaml
+
+aws cloudformation deploy \
+    --template-file ./budget-template.yaml \
+    --stack-name budget-alert
